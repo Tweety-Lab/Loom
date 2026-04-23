8743bb0411d9a0ddab447269779fae82fcf28322
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-04-23 22:10:34 +1000

    Cleanup `ASTWalker` slightly.

--- Loom.Parser/AST/ASTWalker.cs
+++ Loom.Parser/AST/ASTWalker.cs
@@ -7,46 +7,15 @@ namespace Loom.Parser.AST;
 /// </summary>
 public class ASTWalker : ASTVisitor
 {
-    // ewwww...
-
-
-    public override void Visit(BlockNode node)
+    protected void WalkChildren(ASTNode node)
     {
-        base.Visit(node);
-
         foreach (var child in node.Children)
             child.Accept(this);
     }
 
-    public override void Visit(ImportNode node)
-    {
-        base.Visit(node);
-
-        foreach (var child in node.Children)
-            child.Accept(this);
-    }
-
-    public override void Visit(ModuleNode node)
-    {
-        base.Visit(node);
-
-        foreach (var child in node.Children)
-            child.Accept(this);
-    }
-
-    public override void Visit(ProgramNode node)
-    {
-        base.Visit(node);
-
-        foreach (var child in node.Children)
-            child.Accept(this);
-    }
-
-    public override void Visit(UnsafeNode node)
-    {
-        base.Visit(node);
-
-        foreach (var child in node.Children)
-            child.Accept(this);
-    }
+    public override void Visit(BlockNode node) => WalkChildren(node);
+    public override void Visit(ImportNode node) => WalkChildren(node);
+    public override void Visit(ModuleNode node) => WalkChildren(node);
+    public override void Visit(ProgramNode node) => WalkChildren(node);
+    public override void Visit(UnsafeNode node) => WalkChildren(node);
 }
