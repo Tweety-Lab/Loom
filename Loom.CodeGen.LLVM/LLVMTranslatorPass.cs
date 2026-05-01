2218086545f916208e1fc2d71ed7e1599c388dfd
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-05-02 03:14:43 +1000

    Add LLVM `TypeMap`.

--- Loom.CodeGen.LLVM/LLVMTranslatorPass.cs
+++ Loom.CodeGen.LLVM/LLVMTranslatorPass.cs
@@ -14,6 +14,12 @@ public class LLVMTranslatorPass : LIRTranslatorPass<LLVMModuleRef>
     /// <inheritdoc />
     public override void Run(LIRCompilationUnit unit)
     {
+        TypeMap.Add(LIRType.Void, LLVMTypeRef.Void);
+
+        TypeMap.Add(LIRType.Int32, LLVMTypeRef.Int32);
+        TypeMap.Add(LIRType.Boolean, LLVMTypeRef.Int1);
+
+
         Result = LLVMModuleRef.CreateWithName(unit.Name);
     }
 }
---
+++ Loom.CodeGen.LLVM/TypeMap.cs
@@ -0,0 +1,15 @@
+﻿using LLVMSharp.Interop;
+using Loom.LIR;
+
+namespace Loom.CodeGen.LLVM;
+
+internal static class TypeMap
+{
+    /// <summary> Maps LIR types to LLVM types. </summary>
+    public static IReadOnlyDictionary<LIRType, LLVMTypeRef> Map => map;
+
+    private static Dictionary<LIRType, LLVMTypeRef> map = new Dictionary<LIRType, LLVMTypeRef>();
+    
+    /// <summary> Maps a LIR type to an LLVM type. </summary>
+    public static void Add(LIRType type, LLVMTypeRef llvmType) => map.Add(type, llvmType);
+}
