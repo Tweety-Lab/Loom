5ec1f29f202eed3274484a88bfc2e52ebb4332a1
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-05-02 01:19:32 +1000

    Add `LIRPass`.

---
+++ Loom.LIR/Passes/LIRPass.cs
@@ -0,0 +1,31 @@
+﻿
+using Loom.LIR.Objects;
+
+namespace Loom.LIR.Passes;
+
+/// <summary>
+/// Base class for all Loom Intermediate Representation passes.
+/// </summary>
+public abstract class LIRPass
+{
+    /// <summary> Runs the <see cref="LIRPass"/> on the specified <see cref="LIRCompilationUnit"/>. </summary>
+    public virtual void Run(LIRCompilationUnit unit)
+    {
+        foreach (var func in unit.Functions)
+            RunOnFunction(func);
+    }
+
+    protected virtual void RunOnFunction(LIRFunction func)
+    {
+        foreach (var block in func.Blocks)
+            RunOnBlock(block);
+    }
+
+    protected virtual void RunOnBlock(LIRBasicBlock block)
+    {
+        foreach (var inst in block.Instructions.ToList()) // Prevent enumeration issues
+            RunOnInstruction(inst);
+    }
+
+    protected virtual void RunOnInstruction(LIRInstruction inst) { }
+}
